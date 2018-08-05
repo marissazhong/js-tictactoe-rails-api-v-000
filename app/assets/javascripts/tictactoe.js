2f6cf26c6ac603b450438f559e3a19e2d5361697
Marissa Zhong <mwz2101@columbia.edu>
2018-08-05 20:32:30 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,6 +1,6 @@
 // Code your JavaScript / jQuery solution here
 function player(turn) {
-  return (turn % 2 === 0 ? 'X' : 'O')
+  return (turn % 2 === 0 ? 'X' : 'O');
 }
 
 function updateState(board) {
