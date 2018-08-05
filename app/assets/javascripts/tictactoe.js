e81a0652e1ab8a38d7c5668e1be64951da75dfa9
Marissa Zhong <mwz2101@columbia.edu>
2018-08-05 20:41:27 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -3,7 +3,7 @@
 let @@win_combo = [
   [0,1,2],[3,4,5],[6,7,8], // horizontal rows
   [0,3,6],[1,4,7],[2,5,8], // vertical rows
-  [1,4,8],[2,4,6] // diagonals
+  [0,4,8],[2,4,6] // diagonals
 ]
 
 let turn = 0;
