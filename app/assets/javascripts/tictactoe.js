fc0bd22190a8d433a28b34302668285d55a7ec1b
Marissa Zhong <mwz2101@columbia.edu>
2018-08-05 20:44:26 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,6 +1,6 @@
 // Code your JavaScript / jQuery solution here
 
-let @@win_combo = [
+let WIN_COMBO = [
   [0,1,2],[3,4,5],[6,7,8], // horizontal rows
   [0,3,6],[1,4,7],[2,5,8], // vertical rows
   [0,4,8],[2,4,6] // diagonals
